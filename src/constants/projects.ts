8fa2d257aeb5a78e62151dac6dce6211f1734d9d
ThapaSurace <zorosenpai456@gmail.com>
2024-08-11 13:51:32 +0545

    new item added in project

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,31 +3,30 @@ export const projects = [
       id: 1,
       project_name: "Fiverr Clone",
       img: "/images/projects/fiver.png",
-      title: 'A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack',
       desc: "A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack offers a robust and scalable platform for freelancers and clients to connect seamlessly.",
       tags: ["React Js", "Express js", "Mongo DB","Tailwind css"],
       github_link: "https://github.com/ThapaSurace/Fiverr_clone",
       demo_link: "https://freelancerfinder.netlify.app/",
     },
-    {
+  {
       id: 2,
       project_name: "E-Store",
       img: "/images/projects/estore.png",
-      title: 'A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack',
-      desc: "A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack offers a robust and scalable platform for freelancers and clients to connect seamlessly.",
-      tags: ["React Js","Redux Toolkit", "Tailwind css", "Express js", "Mongo DB"],
-      github_link: "https://github.com/ThapaSurace/Fiverr_clone",
-      demo_link: "https://freelancerfinder.netlify.app/",
-    },
+      desc: "E-Store is a full-featured e-commerce platform built with the MERN stack, featuring an intuitive order, payment, and cart system.",
+      tags: ["React Js", "Redux Toolkit", "Tailwind CSS", "Express JS", "MongoDB"],
+      github_link: "https://github.com/ThapaSurace/e-store",
+      demo_link: "https://e-bookofstore.netlify.app/"
+    }
+    ,
     {
       id: 3,
-      project_name: "Fiverr Clone",
-      img: "/images/projects/fiver.png",
-      title: 'A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack',
-      desc: "A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack offers a robust and scalable platform for freelancers and clients to connect seamlessly.",
-      tags: ["Html", "Css", "React Js", "Tailwind css", "Express js", "Mongo DB"],
-      github_link: "https://github.com/ThapaSurace/Fiverr_clone",
-      demo_link: "https://freelancerfinder.netlify.app/",
-    },
+      project_name: "Blog",
+      img: "/images/projects/blog.png",
+      title: "A Blog site built with Next.js, MongoDB, and Tailwind CSS",
+      desc: "This blog site, built with Next.js, MongoDB, and Tailwind CSS, provides a modern and responsive platform for content creation and management. It leverages server-side rendering and efficient data management to deliver a seamless user experience.",
+      tags: ["Html", "Css", "Next.js", "Tailwind CSS", "MongoDB"],
+      github_link: "https://github.com/ThapaSurace/Blog",
+      demo_link: "https://zorovlog.vercel.app/"
+    }
   ];
   
