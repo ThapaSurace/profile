fbf5b22d77ed12da434aeda72dae99511a829079
ThapaSurace <zorosenpai456@gmail.com>
2024-08-11 13:47:12 +0545

    type file made

---
+++ type.d.ts
@@ -0,0 +1,13 @@
+type Project = {
+    id: number;
+    project_name: string;
+    img: string;
+    desc: string;
+    tags: string[];
+    github_link: string;
+    demo_link: string;
+  };
+  
+  type projectProps = {
+    item: Project;
+  };
