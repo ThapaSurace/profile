7439b8b1b74b56798153edc2ad135c38befb8b9f
ThapaSurace <zorosenpai456@gmail.com>
2024-08-13 07:55:04 +0545

    seperator ui added

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,17 +2,21 @@ import { SiCodeproject } from "react-icons/si";
 import Projects from "@/components/Projects";
 import { Suspense } from "react";
 import ProjectSkeleton from "@/components/skeleton/ProjectSkeleton";
+import { Separator } from "@/components/ui/separator";
+
 
 const ProjectPage = () => {
   return (
     <div className="container space-y-10">
       <div>
         <SiCodeproject size={40} className="mb-2" />
-        <h1 className="mb-10">What I've been working on</h1>
+        <h1 className="mb-10">Some of my projects</h1>
       </div>
+      <Separator />
       <Suspense fallback={<ProjectSkeleton />}>
         <Projects />
       </Suspense>
+  
     </div>
   );
 };
