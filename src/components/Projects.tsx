302db42ed752bd58b8d596b9ea460405225a38a7
ThapaSurace <zorosenpai456@gmail.com>
2024-08-17 07:33:02 +0545

    seperator removed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,13 @@
 import { projects } from "@/constants/projects";
 import ProjectCard from "@/components/ProjectCard";
-import { Separator } from "@/components/ui/separator";
 import SlideInFromLeft from "@/components/animation/SlideInFromLeft";
 
 const Projects = () => {
   return (
     <div className="space-y-20">
       {projects.map((item) => (
-        <SlideInFromLeft xOffset={-50} delay={item.id * 0.6}>
+        <SlideInFromLeft key={item.id} xOffset={-50} delay={item.id * 0.6}>
           <ProjectCard item={item} />
-          <Separator className="mt-6" />
         </SlideInFromLeft>
       ))}
     </div>
