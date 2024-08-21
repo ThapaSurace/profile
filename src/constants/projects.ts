a8be9b519dc696d8f22fa2ad75f4abab7b34c63f
ThapaSurace <zorosenpai456@gmail.com>
2024-08-21 07:57:24 +0545

    new project added

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,32 +1,40 @@
 export const projects = [
-    {
-      id: 1,
-      project_name: "Fiverr Clone",
-      img: "/images/projects/fiver.png",
-      desc: "A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack offers a robust and scalable platform for freelancers and clients to connect seamlessly.",
-      tags: ["React Js", "Express js", "Mongo DB","Tailwind css"],
-      github_link: "https://github.com/ThapaSurace/Fiverr_clone",
-      demo_link: "https://freelancerfinder.netlify.app/",
-    },
-  {
-      id: 2,
-      project_name: "E-Store",
-      img: "/images/projects/estore.png",
-      desc: "E-Store is a full-featured e-commerce platform built with the MERN stack, featuring an intuitive order, payment, and cart system.",
-      tags: ["React Js", "Redux Toolkit", "Tailwind CSS", "Express JS", "MongoDB"],
-      github_link: "https://github.com/ThapaSurace/e-store",
-      demo_link: "https://e-bookofstore.netlify.app/"
-    }
-    ,
-    {
-      id: 3,
-      project_name: "Blog",
-      img: "/images/projects/blog.png",
-      title: "A Blog site built with Next.js, MongoDB, and Tailwind CSS",
-      desc: "This blog site, built with Next.js, MongoDB, and Tailwind CSS, provides a modern and responsive platform for content creation and management. It leverages server-side rendering and efficient data management to deliver a seamless user experience.",
-      tags: ["Html", "Css", "Next.js", "Tailwind CSS", "MongoDB"],
-      github_link: "https://github.com/ThapaSurace/Blog",
-      demo_link: "https://zorovlog.vercel.app/"
-    }
-  ];
-  
+	{
+		id: 1,
+		project_name: 'Fiverr Clone',
+		img: '/images/projects/fiver.png',
+		desc: 'A Fiverr clone built with the MERN (MongoDB, Express.js, React, Node.js) stack offers a robust and scalable platform for freelancers and clients to connect seamlessly.',
+		tags: ['React Js', 'Express js', 'Mongo DB', 'Tailwind css'],
+		github_link: 'https://github.com/ThapaSurace/Fiverr_clone',
+		demo_link: 'https://freelancerfinder.netlify.app/',
+	},
+	{
+		id: 2,
+		project_name: 'E-Store',
+		img: '/images/projects/estore.png',
+		desc: 'E-Store is a full-featured e-commerce platform built with the MERN stack, featuring an intuitive order, payment, and cart system.',
+		tags: ['React Js', 'Redux Toolkit', 'Tailwind CSS', 'Express JS', 'MongoDB'],
+		github_link: 'https://github.com/ThapaSurace/e-store',
+		demo_link: 'https://e-bookofstore.netlify.app/',
+	},
+	{
+		id: 3,
+		project_name: 'Blog',
+		img: '/images/projects/blog.png',
+		title: 'A Blog site built with Next.js, MongoDB, and Tailwind CSS',
+		desc: 'This blog site, built with Next.js, MongoDB, and Tailwind CSS, provides a modern and responsive platform for content creation and management. It leverages server-side rendering and efficient data management to deliver a seamless user experience.',
+		tags: ['Html', 'Css', 'Next.js', 'Tailwind CSS', 'MongoDB'],
+		github_link: 'https://github.com/ThapaSurace/Blog',
+		demo_link: 'https://zorovlog.vercel.app/',
+	},
+	{
+		id: 4,
+		project_name: 'Otaku Hub',
+		img: '/images/projects/anime.jpg',
+		title: 'Otaku Hub',
+		desc: 'This anime information website, built with Next.js, Jikan API, and Tailwind CSS, provides detailed information on a wide range of anime series. It features efficient server-side rendering and API integration for seamless data retrieval and a responsive user interface.',
+		tags: ['HTML', 'CSS', 'Next.js', 'Tailwind CSS', 'Jikan API'],
+		github_link: 'https://github.com/ThapaSurace/Anime_info',
+		demo_link: 'https://otakuhub.vercel.app/',
+	},
+];
