e9fcc6d272ccccd414ee735de386482a9c727c5b
Suresh Thapa <zorosenpai456@gmail.com>
2025-01-30 19:36:38 +0545

    commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,16 @@
-import { projects } from '@/constants/projects';
-import ProjectCard from '@/components/ProjectCard';
-import SlideInFromLeft from '@/components/animation/SlideInFromLeft';
+import ProjectCard from "@/components/ProjectCard";
+import SlideInFromLeft from "@/components/animation/SlideInFromLeft";
 
 const Projects = ({ items }: any) => {
-	return (
-		<div className='space-y-20'>
-			{items.map((item: Project) => (
-				<SlideInFromLeft key={item.id} xOffset={-50} delay={item.id * 0.6}>
-					<ProjectCard item={item} />
-				</SlideInFromLeft>
-			))}
-		</div>
-	);
+  return (
+    <div className="space-y-20">
+      {items.map((item: Project) => (
+        <SlideInFromLeft key={item.id} xOffset={-50} delay={item.id * 0.6}>
+          <ProjectCard item={item} />
+        </SlideInFromLeft>
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
