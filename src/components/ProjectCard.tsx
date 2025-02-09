8179abb15a03dd1d2b50c4da0d1de0a37917d9bf
Suresh Thapa <zorosenpai456@gmail.com>
2025-02-09 12:02:47 +0545

    commit

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -22,13 +22,15 @@ const ProjectCard = ({ item }: projectProps) => {
           className="rounded-md object-cover object-center group-hover:opacity-70 transition-opacity duration-300"
         />
         <div className="absolute inset-0 flex justify-center items-center gap-4 opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-500 ease-in-out delay-200">
-          <Link
-            href={item.github_link}
-            className="flex gap-1 items-center py-1 px-2 hover:scale-110 border bg-slate-800 text-white dark:bg-white dark:text-slate-800 transition rounded-md duration-300 ease-in-out"
-          >
-            <span>code</span>
-            <FaGithub size={22} />
-          </Link>
+          {item.github_link && (
+            <Link
+              href={item.github_link}
+              className="flex gap-1 items-center py-1 px-2 hover:scale-110 border bg-slate-800 text-white dark:bg-white dark:text-slate-800 transition rounded-md duration-300 ease-in-out"
+            >
+              <span>code</span>
+              <FaGithub size={22} />
+            </Link>
+          )}
           <Link
             href={item.demo_link}
             className="flex gap-1 items-center py-1 px-2 hover:scale-110 border bg-slate-800 text-white dark:bg-white dark:text-slate-800 transition rounded-md duration-300 ease-in-out"
